cbd859d8a433ab480af1fab82b4e7fac75e96c76
Maksim <familinov@gmail.com>
2020-02-08 21:10:54 +0500

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import MainPage from "../main-screen.jsx";
+import MainPage from "./../main-screen/main-screen.jsx";
+import PropTypes from "prop-types";
 
-const App = (props) => {
-  const {film} = props;
+const App = ({promoFilm, filmList}) => {
   return (
-    <MainPage film={film} />
+    <MainPage promoFilm={promoFilm} filmList={filmList}/>
   );
-}
+};
+
+App.propTypes = {
+  filmList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  promoFilm: PropTypes.exact({
+    filmName: PropTypes.string.isRequired,
+    filmGenre: PropTypes.string.isRequired,
+    filmYear: PropTypes.number.isRequired
+  }).isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+export const FilmList = [
+  `Battlefield Earth: A Saga of the Year 3000`,
+  `Epic Movie`,
+  `Santa Claus Conquers the Martians`,
+  `United Passions`,
+  `The Room`,
+  `Howard the Duck`,
+  `Showgirls`,
+  `The Exorcist II: The Heretic`,
+  `Jack and Jill`,
+  `Movie 43`,
+  `Cannibal Holocaust`,
+  `The Human Centipede III`
+];
+
 const PromoFilm = {
   filmName: `The Grand Budapest Hotel`,
   filmGenre: `Drama`,
@@ -9,6 +24,6 @@ const PromoFilm = {
 };
 
 ReactDOM.render(
-    <App film={PromoFilm}/>,
+    <App promoFilm={PromoFilm} filmList={FilmList}/>,
     document.querySelector(`#root`)
 );
