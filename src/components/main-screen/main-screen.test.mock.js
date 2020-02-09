ab0549cd1d4032d7b51749997adce78ff9a2b64c
Maksim <familinov@gmail.com>
2020-02-09 14:39:58 +0500

    module2-task2

---
+++ src/components/main-screen/main-screen.test.mock.js
@@ -0,0 +1,11 @@
+export const FilmListTest = [
+  `The Godfather`,
+  `The Shawshank Redemption`,
+  `Schindler's List`
+];
+
+export const PromoFilmTest = {
+  filmName: `The Wizard of Oz`,
+  filmGenre: `Adventure`,
+  filmYear: 1939
+};
