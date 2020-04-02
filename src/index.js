fbae92ca243432a09faa3f74a54aae8b5ba140e7
Maksim <familinov@gmail.com>
2020-04-02 18:05:34 +0500

    module7-task2 without tests

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 import {Provider} from "react-redux";
 import {reducer} from "./reducer/reducer.js";
-import {Operation} from "./reducer/reducer.js";
+import {DataOperation, UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/reducer.js";
 import {createAPI} from "./api.js";
 
-const api = createAPI(()=>{});
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
@@ -18,7 +22,8 @@ const store = createStore(
     )
 );
 
-store.dispatch(Operation.loadMovies());
+store.dispatch(DataOperation.loadMovies());
+store.dispatch(UserOperation.checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/reducer/movie/reducer.js
@@ -0,0 +1,70 @@
+import {PromoFilm} from "../../mocks/films.js";
+
+const DEFAULT_SHOW_NUBMER = 8;
+
+const initialState = {
+  genre: `All genres`,
+  promoFilm: PromoFilm,
+  currentMovie: null,
+  isPlaying: false,
+  shownCount: DEFAULT_SHOW_NUBMER,
+};
+
+const ActionType = {
+  GENRE_CHANGED: `GENRE_CHANGED`,
+  CURRENT_MOVIE_CHANGED: `CURRENT_MOVIE_CHANGED`,
+  RESET_STORE: `RESET_STORE`,
+  SHOW_MORE: `SHOW_MORE`,
+  PLAY_MOVIE: `PLAY_MOVIE`,
+};
+
+const changeGenre = (genre) => ({
+  type: ActionType.GENRE_CHANGED,
+  payload: genre
+});
+
+const changeCurrentMovie = (id) => ({
+  type: ActionType.CURRENT_MOVIE_CHANGED,
+  payload: id
+});
+
+const changeFilmsCount = () => ({
+  type: ActionType.SHOW_MORE,
+  payload: DEFAULT_SHOW_NUBMER
+});
+
+const playPauseMovie = (boolean) => ({
+  type: ActionType.PLAY_MOVIE,
+  payload: boolean
+});
+
+const resetStore = () => ({
+  type: ActionType.RESET_STORE
+});
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case ActionType.GENRE_CHANGED:
+      return {...state, genre: action.payload, shownCount: DEFAULT_SHOW_NUBMER};
+
+    case ActionType.CURRENT_MOVIE_CHANGED:
+      const currentMovie = state.movieList.find((movie) =>
+        parseInt(movie.id, 10) === parseInt(action.payload, 10)
+      );
+      return {...state, currentMovie};
+
+    case ActionType.SHOW_MORE:
+      return {...state, shownCount: state.shownCount + action.payload};
+
+    case ActionType.PLAY_MOVIE:
+      return {...state, isPlaying: action.payload};
+
+    case ActionType.RESET_STORE:
+      return {...state, genre: `All genres`, currentMovie: null, shownCount: DEFAULT_SHOW_NUBMER};
+  }
+
+  return state;
+};
+
+export {reducer, ActionType, changeGenre, changeCurrentMovie, changeFilmsCount, playPauseMovie, resetStore};
